513e730128998219786cbfec2fb8148d45b9dcf0
Andrews, Taylor <taylor.andrews0391@gmail.com>
2025-06-26 20:45:03 -0400

    Testing smote fix

--- pipeline_gpu.py
+++ pipeline_gpu.py
@@ -41,11 +41,8 @@ def handle_class_imbalance(X, y, strategy='smote'):
 
         return X, y, class_weights
 
-    # X_pandas = X.to_pandas()
-    # y_pandas = y.to_pandas()
-
-    X_pandas = X
-    y_pandas = y
+    X_pandas = pd.DataFrame(X.get())
+    y_pandas = pd.DataFrame(y.get())
 
     if strategy == 'smote':
         smote = SMOTE(random_state=42)
